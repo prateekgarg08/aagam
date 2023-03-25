3760bf87f04d03af13e3ead8f4e28c1a43862d1a
prateekgarg08 <prateekgarg881@gmail.com>
2023-03-25 13:23:57 +0530

    sticky nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,25 +5,30 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./public/**/*.{png,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       // run 
-      backgroundImage:{
-       'br':"url('./regbg.png')",
+      backgroundImage: {
+        'br': "url('./regbg.png')",
       },
-      height:{
-        extra:'32rem'
+      height: {
+        extra: '32rem'
       },
       borderRadius: {
         extraLarge: '12rem'
       },
-      screens:{
-        'sm':{'max':'800px'},
+      screens: {
+        'sm': { 'max': '800px' },
         // 'extra':{'max'}
+      },
+      colors: {
+        orange: "#EF542E",
+        bgBlack: "black"
+
       }
 
     },
