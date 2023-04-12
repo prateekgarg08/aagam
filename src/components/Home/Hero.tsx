3a0f08c8f42cfde175e4a89567c0eb459b373835
kishan bhati <bhatikaran634@gmail.com>
2023-04-12 17:58:49 +0530

    nav

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -5,21 +5,21 @@ import { useRouter } from 'next/router'
 export const Hero: FC<{}> = ({}) => {
   const router = useRouter()
   return (
-    <div className="flex bg-black flex-col w-full my-20 py-5 justify-center items-center space-y-10">
+    <div className="flex bg-black flex-col w-full  justify-center items-center space-y-10">
       <Image
-        src="/AAGAMLOGO.png"
-        width={530}
-        height={300}
+        src="/AAGAMLOGO1.png"
+        width={630}
+        height={400}
         alt="banner"
       />
-      <button
+      {/*   <button
         onClick={() => {
           router.push('/register')
         }}
-        className="flex items-center gap-3 font-semibold text-white rounded-full px-3 py-2 bg-gradient-to-r from-primary to-secondary hover:from-secondary hover:to-primary duration-300 transition-all"
+        className="flex  items-center gap-3 font-semibold text-white rounded-full px-3 py-2 bg-gradient-to-r text-2xl from-primary to-secondary hover:from-secondary hover:to-primary duration-300 transition-all"
       >
-        Register Now
-      </button>
+        Register
+      </button> */}
     </div>
   )
 }
